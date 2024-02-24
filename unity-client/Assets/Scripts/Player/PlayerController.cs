a108b49372971775021f8c58f289041594775310
migueldiasbrito <migueldiasbrito@gmail.com>
2024-02-24 10:14:55 +0000

    Add movement to Player

--- unity-client/Assets/Scripts/Player/PlayerController.cs
+++ unity-client/Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Action<PlayerController> _onReadyCallback;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _speed;
 
-    public void SetOnReadyCallback(Action<PlayerController> onReadyCallback)
-    {
-        _onReadyCallback = onReadyCallback;
-    }
+    private Vector2 _movement;
+
+    private Action<PlayerController> _onReadyCallback;
 
     public void OnPlayerReady(InputAction.CallbackContext callbackContext)
     {
         if (!callbackContext.performed) return;
 
         _onReadyCallback.Invoke(this);
+    }
+
+    public void OnMove(InputAction.CallbackContext callbackContext)
+    {
+        _movement = callbackContext.ReadValue<Vector2>();
+    }
+
+    public void SetOnReadyCallback(Action<PlayerController> onReadyCallback)
+    {
+        _onReadyCallback = onReadyCallback;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = _rigidbody.velocity;
+
+        velocity.x = _movement.x * _speed * Time.fixedDeltaTime;
+        velocity.z = _movement.y * _speed * Time.fixedDeltaTime;
+
+        _rigidbody.velocity = velocity;
     }
 }
