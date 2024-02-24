5bca2d1139ed3e8c41a3ce91c4976cd8cd5e256d
Moisés <liricractos@gmail.com>
2024-02-24 09:35:41 +0000

    my up

---
+++ unity-client/Assets/Scripts/Player/Animation_reaction.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_reaction : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("walkSpeed", Mathf.Abs(rigidbody.velocity.x));
+        //if(transform.forward == Vector3)
+    }
+
+    public void Sit()
+    {
+        animator.SetBool("doSit", true);
+    }
+}
