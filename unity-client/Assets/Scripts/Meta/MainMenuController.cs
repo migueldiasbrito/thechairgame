8ea9f0c5da030b6d26787f26d45e9ae123ed4b27
migueldiasbrito <migueldiasbrito@gmail.com>
2024-02-25 11:18:49 +0000

    HELP

--- unity-client/Assets/Scripts/Meta/MainMenuController.cs
+++ unity-client/Assets/Scripts/Meta/MainMenuController.cs
@@ -5,6 +5,7 @@ public class MainMenuController : MonoBehaviour
 {
     [SerializeField] private int[] _gameScenesIndexes = { 2, 3, 4 };
     [SerializeField] private int _creditsSceneIndex = 1;
+    [SerializeField] private int _helpSceneIndex = 5;
 
     public void GoToGame()
     {
@@ -15,6 +16,11 @@ public class MainMenuController : MonoBehaviour
     public void GoToCredits()
     {
         SceneManager.LoadScene(_creditsSceneIndex);
+    }
+
+    public void GoToHelp()
+    {
+        SceneManager.LoadScene(_helpSceneIndex);
     }
 
     public void OnExit()
